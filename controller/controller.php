f092e2c7733411dce251dd25fd8d867b81681ecf
n4nell <khairinnaila08@gmail.com>
2025-09-09 07:31:19 +0700

    controller lagi di isi

--- controller/controller.php
+++ controller/controller.php
@@ -1,1 +1,10 @@
 <?php 
+include_once __DIR__."/../model/model.php";
+
+class Controller extends Model{
+    function getAktris(){ return $this->findAll(); }
+
+    function getAktrisById($id){
+        
+    }
+}
